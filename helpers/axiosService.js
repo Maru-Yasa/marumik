43ddfefea7daab90c44e096b0ab4ac1906f061df
maruf <marufilyasa13@gmail.com>
2021-06-14 08:32:01 +0700

    repair axios service2

--- helpers/axiosService.js
+++ helpers/axiosService.js
@@ -14,39 +14,32 @@ const cookiejar = new tough.CookieJar();
 // });
 axios.defaults.baseURL = baseUrl;
 // axios.defaults.httpsAgent = tunnelAgent;
-// axios.defaults.jar = cookiejar;
+axios.defaults.jar = cookiejar;
 // 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1';
-// const AxiosService = async (url) => {
-//   return new Promise(async (resolve, reject) => {
-//     const _url = url == null?url:encodeURI(url);
-//     try {
-//       const response = await axios.get(_url,{
-//         headers:{
-//           'User-Agent':randomUserAgent('desktop')
-//         }
-//       });
-//       if (response.status === 200) {
-//         return resolve(response);
-//       }
-//       return reject(response);
-//     } catch (error) {
-//       return reject(error.message);
-//     }
-//   });
-// };
-
 const AxiosService = async (url) => {
-  try {
-    var response = await axios.get(url, {
-      headers: { 'User-Agent':randomUserAgent()}
-    });
-    console.log(response.config.headers)
-    return response
-  } catch (error) {
-    console.log(error)
-    return undefined
-  }
-}
+  return new Promise(async (resolve, reject) => {
+    const _url = url == null?url:encodeURI(url);
+    try {
+      const response = await axios.get(_url);
+      if (response.status === 200) {
+        return resolve(response);
+      }
+      return reject(response);
+    } catch (error) {
+      return reject(error.message);
+    }
+  });
+};
+
+// const AxiosService = async (url) => {
+//   try {
+//     var response = await axios.get(url);
+//     return response
+//   } catch (error) {
+//     console.log(error)
+//     return undefined
+//   }
+// }
 
 
 
